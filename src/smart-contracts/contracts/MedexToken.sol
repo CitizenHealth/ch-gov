cc32066623e9e446641ce92c56c6aff1cc2ff3ba
James Cunningham <j.a.cunningham@gmail.com>
2018-03-08 15:57:07 +0000

    Add initial Medex Token implementation

---
+++ src/smart-contracts/contracts/MedexToken.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/token/ERC827/ECR827Token.sol';
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+
+contract MedexToken is ERC827Token, MintableToken {
+  string public constant name = "Pre-medex";
+  string public constant symbol = "MDX";
+  string public constant decimals = 18;
+
+  uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(decimals));
+
+  function MedexToken () public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  }
+}
