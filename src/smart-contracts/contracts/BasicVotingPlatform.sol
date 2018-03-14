153822d32362bcfa6e0a101f954dde948612dd80
James Cunningham <j.a.cunningham@gmail.com>
2018-03-14 12:49:54 +0000

    Refactor VotingPlatform contract

---
+++ src/smart-contracts/contracts/BasicVotingPlatform.sol
@@ -0,0 +1,90 @@
+pragma solidity ^0.4.18;
+
+import './VotingPlatform.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
+contract BasicVotingPlatform is VotingPlatform {
+  using SafeMath for uint256;
+
+  Ballot[] public ballots;
+  mapping(uint256 => Proposal[]) public proposals;
+  mapping(address => mapping (uint256 => bool)) public voted;
+
+  function createBallot(bytes32 _title) public returns (uint256) {
+    uint256 _id = ballots.length;
+    ballots.push(Ballot({id: _id,
+                         title: _title,
+                         creator: msg.sender}));
+    
+    BallotCreated(msg.sender, _id, _title);    
+    return _id;
+  }
+
+  function createProposal(uint256 _ballotId, string _proposal) public returns (uint256) {
+    require(_ballotId < ballots.length);
+    require(ballotStatus(_ballotId) == Status.AMENABLE);
+
+    uint256 _id = proposals[_ballotId].length;
+    proposals[_ballotId].push(Proposal({id: _id,
+                                        proposal: _proposal,
+                                        proposer: msg.sender,
+                                        votes: 0}));
+    
+    ProposalCreated(msg.sender, _ballotId, _proposal);    
+    return _id;
+  }
+
+  function getVotesFor(address voter) public returns (uint256);
+
+  function vote(uint256 _ballotId, uint256 _proposalId) public returns (bool) {
+    require(_ballotId < ballots.length);    
+    require(ballotStatus(_ballotId) == Status.OPEN);
+    require(proposals[_ballotId].length < _proposalId);
+    require(!voted[msg.sender][_ballotId]);
+
+    uint256 votes = getVotesFor(msg.sender);
+    proposals[_ballotId][_proposalId].votes.add(votes);
+    voted[msg.sender][_ballotId] = true;
+
+    Voted(msg.sender, _ballotId, _proposalId, votes);
+    return true;
+  }
+  
+  function ballotResult(uint256 _ballotId) public view returns (Result, Proposal[]) {
+    require(ballots.length < _ballotId);
+    
+    if(ballotStatus(_ballotId) != Status.CLOSED) {
+      return (Result.UNDECIDED, new Proposal[](0));
+    }
+    
+    uint256 max = 0;
+    uint256 count = 0;
+    for(uint i = 0; i < proposals[_ballotId].length; i++) {
+      var votes = proposals[_ballotId][i].votes;
+      if(votes > max) {
+        max = votes;
+        count = 1;
+      } else if(votes == max) {
+        count = count.add(1);
+      }
+    }
+    uint256 done = 0;
+    Proposal[] memory props = new Proposal[](count);
+    for(uint j = 0; j < proposals[_ballotId].length; j++) {
+      Proposal memory next = proposals[_ballotId][j];
+      if(next.votes == max) {
+        props[done] = Proposal({id: next.id,
+                                proposal: next.proposal,
+                                proposer: next.proposer,
+                                votes: next.votes});
+        done++;
+      }
+    }
+    if(count < 2) {
+      return (Result.DECIDED, props);
+    } else {
+      return (Result.TIED, props);
+    }
+  }
+
+}
--- src/smart-contracts/contracts/VotingPlatform.sol
+++ src/smart-contracts/contracts/VotingPlatform.sol
@@ -1,63 +1,55 @@
 pragma solidity ^0.4.18;
 
 import './PeriodicToken.sol';
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract VotingPlatform {
-  using SafeMath for uint256;
 
   PeriodicToken public backingToken;
   uint256 public ballotDeposit;
   uint256 public proposalDeposit;
   mapping (uint256 => Ballot) public ballots;
   uint256 internal id_;
+  
+  enum Status {
+    AMENABLE,
+    OPEN,
+    CLOSED
+  }
 
-  struct Proposal {    
+  enum Result {
+    UNDECIDED,
+    DECIDED,
+    TIED
+  }
+
+  struct Proposal {
+    uint256 id;
     string proposal;
     address proposer;
-    uint256 deposited;
+    // uint256 deposited;
     uint256 votes;
   }
   
   struct Ballot {
     uint256 id;
     bytes32 title;
-    uint256 start;
     address creator;
-    uint256 deposited;
-    Proposal[] proposals;
+    // uint256 deposited;
   }
 
-  function createBallot(bytes32 _title) public returns (uint256) {
-    require(backingToken.transferFrom(msg.sender, address(this), ballotDeposit));
-    uint256 id = id_;
-    id_ = id_.add(1);
-    ballots[id].id = id;
-    ballots[id].start =  backingToken.currentPeriod().add(1);
-    ballots[id].creator = msg.sender;
-    ballots[id].deposited = ballotDeposit;
-    BallotCreated(id, _title);
-    return id;
-  }
+  function createBallot(bytes32 _title) public returns (uint256);
 
-  function reclaimBallotDeposit(uint256 _period, uint256 _id) public returns (bool) {
-    require(_period < backingToken.currentPeriod());
-    require(ballots[_id].creator == msg.sender);
-    return backingToken.transfer(ballots[_id].creator, ballots[_id].deposited);
-  }
+  function createProposal(uint256 _ballotId, string _proposal) public returns (uint256);
 
-  function createProposal(uint256 _ballotId, string _proposal) public returns (bool) {
-    require(_ballotId < id_);
-    require(ballots[_ballotId].start > backingToken.currentPeriod());
-    require(backingToken.transferFrom(msg.sender, address(this),
-                                      proposalDeposit));
-    var prop = Proposal({proposal: _proposal,
-                         proposer: msg.sender,
-                         deposited: proposalDeposit,
-                         votes: 0});
-    ballots[_ballotId].proposals.push(prop);
-    return true;    
-  }
+  function vote(uint256 _ballotId, uint256 _proposalId) public returns (bool);
 
-  event BallotCreated(uint256 indexed id, bytes32 title);
+  function ballotStatus(uint256 _ballotId) public view returns (Status);
+
+  function ballotResult(uint256 _ballotId) public view returns (Result, Proposal[]);
+
+  event BallotCreated(address indexed creator, uint256 indexed id, bytes32 title);
+  
+  event ProposalCreated(address indexed creator, uint256 indexed ballotId, string proposal);
+  
+  event Voted(address indexed voter, uint256 indexed ballot, uint256 indexed proposal, uint256 votes);
 }
